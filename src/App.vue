9731cfc9e3a595462b452a251fdb8a7acbfedad2
Jack Coble <jack@coble.uk>
2021-03-02 16:53:04 +0000

    Start of Navigation bar component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
+  <navigation />
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import Navigation from "@/components/Navigation.vue";
+
+export default defineComponent({
+  components: {
+    Navigation
+  }
+});
+</script>

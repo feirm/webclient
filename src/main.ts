108959877bc51d8a96cf620b8ec867850c24e728
Jack Coble <jack@coble.uk>
2021-03-02 13:11:42 +0000

    Start of a global stylesheet

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// CSS Styles
+import "@/css/global.css";
+
 createApp(App)
   .use(store)
   .use(router)

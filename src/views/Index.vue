399501e1398d9a9f2da56deb0d2bad7a222eb7c0
Jack Coble <jack@coble.uk>
2021-03-13 16:28:25 +0000

    Update Index page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-    <h2>AN ECOSYSTEM WHERE PRIVACY IS FOR EVERYONE.</h2>
-    <p>
-      A blockchain project and platform built for your privacy. Access Feirm
-      services, store your contacts, and create a non-custodial cryptocurrency
-      wallet.
-    </p>
-  </div>
+  <!-- Landing hero -->
+  <h1 class="text-5xl mb-3">
+    An ecosystem where privacy is for everyone.
+  </h1>
+  <p>
+    A blockchain project and platform built for your privacy. Access Feirm
+    services, store your contacts, and create a non-custodial cryptocurrency
+    wallet.
+  </p>
 </template>
 
 <script lang="ts">

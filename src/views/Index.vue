7c2607148301e8ae1f6cb55544dd7fb631aebeb3
Jack Coble <jack@coble.uk>
2021-03-13 22:35:07 +0000

    Styling of hero component on landing page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- Landing hero -->
-  <h1 class="text-5xl mb-3">
-    An ecosystem where privacy is for everyone.
-  </h1>
-  <p>
-    A blockchain project and platform built for your privacy. Access Feirm
-    services, store your contacts, and create a non-custodial cryptocurrency
-    wallet.
-  </p>
+  <div class="p-10 sm:p-40 bg-grey-500">
+    <div class="md:ml-60">
+      <h1 class="md:w-2/4 text-4xl md:text-5xl leading-tight mb-3 text-orange">An ecosystem where privacy is for everyone.</h1>
+      <p class="md:w-1/3 text-2xl text-gray-100">
+        A blockchain project and platform built for your privacy. Access Feirm
+        services, store your contacts, and create a non-custodial cryptocurrency
+        wallet. Privacy and security by design.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">

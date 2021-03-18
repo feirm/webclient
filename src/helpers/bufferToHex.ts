17eac9169e1fa633159f86b3a711e6cf242ea9f3
Jack Coble <jack@coble.uk>
2021-03-18 20:29:24 +0000

    Helper functions

---
+++ src/helpers/bufferToHex.ts
@@ -0,0 +1,5 @@
+export default function bufferToHex(buffer: ArrayBuffer): string {
+  return Array.prototype.map
+    .call(new Uint8Array(buffer), (x) => ("00" + x.toString(16)).slice(-2))
+    .join("");
+}

98ea12059112bbed9b0d93635fba812a17ccad25
Jack Coble <jack@coble.uk>
2021-03-19 19:04:18 +0000

    Router logged in check

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,7 @@ import VerifyEmail from "@/views/app/auth/VerifyEmail.vue";
 
 // Bridge
 import BridgeIndex from "@/views/app/bridge/Index.vue";
+import { store } from "@/store";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -65,9 +66,31 @@ const routes: Array<RouteRecordRaw> = [
   }
 ];
 
+// Create a router instance
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
+// For every route, check if the user is authenticate
+router.beforeEach((to, from, next) => {
+  const loggedIn = store.getters.isLoggedIn;
+  console.log("[Router] Logged in:", loggedIn);
+
+  // If the user is not logged in, but has a refresh token, let them continue
+  if (!loggedIn && store.getters.getRefreshToken) {
+    return next();
+  }
+
+  // If the user is not logged in and has no refresh token, redirect them to
+  // the login page
+  if (!loggedIn && !store.getters.getRefreshToken) {
+    return next("/app/login");
+  }
+
+  // Otherwise all them to go as usual
+  return next();
+});
+
+// Export the router
 export default router;

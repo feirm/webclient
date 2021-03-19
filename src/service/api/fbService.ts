55df0104191594af8698283ab83d18181d07137b
Jack Coble <jack@coble.uk>
2021-03-19 16:41:20 +0000

    Firebase API axios service

---
+++ src/service/api/fbService.ts
@@ -0,0 +1,11 @@
+import { firebaseApi } from "./api"
+
+// Firebase authentication service
+export default {
+    getNewIdToken(refreshToken: string) {
+        return firebaseApi.post("", {
+            grant_type: "refresh_token",
+            refresh_token: refreshToken
+        })
+    }
+}

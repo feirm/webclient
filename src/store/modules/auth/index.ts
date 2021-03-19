41b9e551f3c1f70b8e0487bd6dc34c40bd08d442
Jack Coble <jack@coble.uk>
2021-03-19 16:22:10 +0000

    Update Vuex auth module to support refresh tokens

--- src/store/modules/auth/index.ts
+++ src/store/modules/auth/index.ts
@@ -2,27 +2,34 @@ export const auth = {
     // Authentication module state
     state: {
         auth: {
-            idToken: localStorage.getItem("idToken") || ""
+            idToken: localStorage.getItem("idToken") || "",
+            refreshToken: localStorage.getItem("refreshToken") || ""
         }
     },
 
     // Mutations
     mutations: {
-        setIdToken(state: any, idToken: any) {
+        setIdToken(state: any, idToken: string) {
             state.auth.idToken = idToken;
             localStorage.setItem("idToken", idToken);
+        },
+        setRefreshToken(state: any, refreshToken: string) {
+            state.auth.refreshToken = refreshToken;
+            localStorage.setItem("refreshToken", refreshToken);
         }
     },
 
     // Actions
     actions: {
-        login({ commit }, idToken: any) {
+        login({ commit }, { idToken, refreshToken }) {
             commit("setIdToken", idToken);
+            commit("setRefreshToken", refreshToken);
         }
     },
 
     // Getters
     getters: {
-        getIdToken: (state: any) => state.auth.idToken
+        getIdToken: (state: any) => state.auth.idToken,
+        getRefreshToken: (state: any) => state.auth.refreshToken
     }
 }

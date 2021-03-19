b8e1f930bf8c25b7508a62c37fc034ebc8ffd38a
Jack Coble <jack@coble.uk>
2021-03-19 13:55:25 +0000

    Initialise Firebase

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Firebase
+import firebase from "firebase/app";
+import "firebase/auth";
+
 // CSS Styles (Tailwind)
 import "@/css/index.css";
+
+// Firebase Authentication
+const firebaseConfig = {
+  apiKey: "AIzaSyBKPqBFtNyw_P1kdz48JTW2Zqm5Vvrn8E0",
+  authDomain: "auth.feirm.com",
+  projectId: "feirm-b4e50"
+}
+
+// Initialise Firebase
+firebase.initializeApp(firebaseConfig)
 
 createApp(App)
   .use(store)

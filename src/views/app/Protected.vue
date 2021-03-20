405e76cc307f6343f00d4e7493196b1ae10a58d5
Jack Coble <jack@coble.uk>
2021-03-20 15:40:55 +0000

    Testing - show encrypted account from IDB

--- src/views/app/Protected.vue
+++ src/views/app/Protected.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
     <h1>Protected resource!</h1>
+    {{ account }}
   </div>
 </template>
 
 <script lang="ts">
+import account from "@/class/account";
 import { defineComponent } from "@vue/runtime-core";
 
 export default defineComponent({
-  name: "Protected"
+  name: "Protected",
+  data() {
+    return {
+      account: null
+    }
+  },
+  async mounted() {
+    this.account = await account.fetchAccount();
+  }
 });
 </script>

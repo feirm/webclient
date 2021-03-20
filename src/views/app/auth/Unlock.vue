1f4efe4912d5e1032e91503c70cccd91c1b6b926
Jack Coble <jack@coble.uk>
2021-03-20 17:04:08 +0000

    Improved unlock route styling

--- src/views/app/auth/Unlock.vue
+++ src/views/app/auth/Unlock.vue
@@ -1,8 +1,32 @@
 <template>
-    <div>
-        <h1>Unlock account</h1>
-        <input type="password" v-model="encryptionKey">
-        <button @click="decrypt">Unlock</button>
+    <div class="min-h-screen flex items-center justify-center bg-grey-500">
+        <div class="bg-gray-100 p-8 shadow-2xl sm:w-1/2 md:w-1/2 lg:w-1/4 sm:rounded md:rounded lg:rounded sm:h-full md:h-full lg:h-full">
+            <router-link to="/">
+                <img
+                    class="mx-auto w-16 mb-5"
+                    src="@/assets/img/logo.png"
+                    alt="Feirm Logo"
+                />
+            </router-link>
+
+            <h1 class="text-3xl font-light mb-4 text-center">Unlock your account 🔒</h1>
+            <p class="font-light mb-3">Please enter your encryption key to unlock your Feirm account.</p>
+
+             <!-- Encryption key input -->
+            <label class="block mb-2 font-light text-gray-500">Encryption Key</label>
+            <input
+                class="w-full mb-3 border-2 border-gray-200 p-3 rounded outline-none focus:border-orange-500 transition duration-200"
+                v-model="encryptionKey"
+                type="password"
+                autofocus
+            />
+
+            <!-- Decrypt button -->
+            <button class="block w-full bg-orange-500 hover:bg-orange-400 p-4 rounded text-yellow-900 transition duration-300" @click="decrypt()">
+              <p v-if="!decrypting">Unlock</p>
+              <img v-else class="mx-auto w-6" src="@/assets/loading_spinner.svg" alt="Loading spinner"/>
+            </button>
+        </div>
     </div>
 </template>
 
@@ -16,7 +40,8 @@ export default defineComponent({
     name: "Unlock",
     data() {
         return {
-            encryptionKey: ""
+            encryptionKey: "",
+            decrypting: false
         }
     },
     computed: {
@@ -26,6 +51,8 @@ export default defineComponent({
     },
     methods: {
         async decrypt() {
+            this.decrypting = true;
+
             try {
                 // Fetch encrypted account from IDB
                 const encryptedAccount = await account.fetchAccount();
@@ -34,6 +61,7 @@ export default defineComponent({
                 const rootKey = await account.decryptRootKey(this.encryptionKey, encryptedAccount);
                 account.setRootKey(rootKey);
             } catch (e) {
+                this.decrypting = false;
                 return this.$toast.error("Failed to unlock your account!")
             }
 
@@ -48,8 +76,14 @@ export default defineComponent({
                 this.store.dispatch("login", { idToken, refreshToken });
             } catch (e) {
                 console.log(e);
+
+                this.decrypting = false;
                 return this.$toast.error("Unable to fetch access token!")
             }
+
+            this.decrypting = false;
+
+            // TODO: Redirect back to original page
         }
     },
     setup() {

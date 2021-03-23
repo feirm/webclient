067bc45e9b920b4de6eb00bec12f7cc5f33ff3d9
Jack Coble <jack@coble.uk>
2021-03-23 21:31:56 +0000

    Update index page content

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,12 @@
 <template>
   <!-- Landing hero -->
   <div class="p-10 sm:p-40 bg-grey-500">
-    <div class="md:ml-60">
-      <h1 class="md:w-2/4 text-4xl md:text-5xl leading-tight mb-3 text-orange">
+    <div>
+      <h1 class="md:w-2/4 mx-auto text-4xl text-center md:text-6xl mb-10 text-orange">
         An ecosystem where privacy is for everyone.
       </h1>
-      <p class="md:w-1/3 text-2xl text-gray-100">
-        A blockchain project and platform built for your privacy. Access Feirm
-        services, store your contacts, and create a non-custodial cryptocurrency
-        wallet. Privacy and security by design.
+      <p class="md:w-1/3 mx-auto text-2xl text-gray-100">
+        A blockchain project and platform built for your privacy. Take a deep dive into the Feirm Ecosystem and experience the ultimate privacy and security provided by design.
       </p>
     </div>
   </div>

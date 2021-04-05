24bcfa1c80ea68044c278ee1d3f52e8223976aac
Jack Coble <jack@coble.uk>
2021-04-05 20:02:57 +0100

    Update Axios authentication service

--- src/service/api/authService.ts
+++ src/service/api/authService.ts
@@ -1,11 +1,19 @@
 import { gatewayApi } from "@/service/api/api";
-import { EncryptedKey } from "@/models/account";
+import { EncryptedAccount } from "@/models/account";
 
 export default {
-  // Send encrypted key payload
-  SendKey(key: EncryptedKey) {
-    return gatewayApi.post("auth/v1/add_key", key);
+  // Check username availability
+  CheckUsername(username: string) {
+    return gatewayApi.post("auth/v1/checkusername", {
+      username
+    });
   },
+
+  // Send encrypted account payload
+  SendKey(account: EncryptedAccount) {
+    return gatewayApi.post("auth/v1/register", account);
+  },
+
   // Get encrypted account payload
   GetAccount() {
     return gatewayApi.get("auth/v1/get_account");

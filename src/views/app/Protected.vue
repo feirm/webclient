c9e8de1a06b65d98f46f0bffe756c5604c4c136a
Jack Coble <jack@coble.uk>
2021-04-13 20:40:12 +0100

    Basic authorised page

--- src/views/app/Protected.vue
+++ src/views/app/Protected.vue
@@ -1,23 +1,18 @@
 <template>
   <div>
     <h1>Protected resource!</h1>
-    {{ account }}
   </div>
 </template>
 
 <script lang="ts">
-import account from "@/class/account";
 import { defineComponent } from "@vue/runtime-core";
 
 export default defineComponent({
   name: "Protected",
   data() {
     return {
-      account: null
+      account: ""
     };
   },
-  async mounted() {
-    this.account = await account.fetchAccount();
-  }
 });
 </script>

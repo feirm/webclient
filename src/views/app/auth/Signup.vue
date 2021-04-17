a37a3c385fa48b0366a0c213417f0399ba3eeae4
Jack Coble <jack@coble.uk>
2021-04-17 16:56:13 +0100

    Remove two-factor modal from signup page

--- src/views/app/auth/Signup.vue
+++ src/views/app/auth/Signup.vue
@@ -24,6 +24,16 @@
           </p>
 
           <form v-on:submit.prevent="checkForm" class="space-y-3">
+            <!-- Email input -->
+            <label class="block text-gray-900">Email address</label>
+            <input
+              class="w-full border-2 border-gray-200 p-2 md:p-3 rounded outline-none focus:border-orange-500 transition duration-200"
+              v-model="email"
+              type="text"
+              placeholder="Please enter an email address"
+              autofocus
+            />
+
             <!-- Username input -->
             <label class="block text-gray-900">Username</label>
             <input
@@ -31,7 +41,6 @@
               v-model="username"
               type="text"
               placeholder="Please pick a username"
-              autofocus
               v-on:input="usernameValid($event.target.value)"
             />
 
@@ -101,96 +110,16 @@
             <router-link class="text-orange" to="/privacy">privacy policy</router-link> enforced throughout our projects.
           </p>
 
-          <button class="block w-full p-3 rounded shadow bg-orange-500 text-yellow-900">I agree</button>
+          <button @click="register" class="block w-full p-3 rounded shadow bg-orange-500 text-yellow-900">I agree</button>
           <router-link to="/" class="block w-full p-3 rounded shadow bg-red-400 text-red-900 text-center">I do not agree</router-link>
         </div>
       </div>
     </div>
-
-    <!-- Two factor authentication modal setup -->
-    <div
-      v-if="showTwoFactorSetup"
-      class="fixed overflow-x-hidden overflow-y-auto inset-0 z-50 p-8"
-    >
-      <div class="relative mx-auto w-2/6 max-w-2xl">
-        <div
-          class="flex flex-col bg-white rounded shadow-2xl w-full p-8 space-y-4"
-        >
-          <h1 class="text-light text-3xl text-center">
-            Two Factor Authentication (2FA)
-          </h1>
-          <p>
-            As an additional layer of security, the Feirm Platform enforces the
-            use of two-factor authentication. This is used to protect your
-            account from fraudulent access and to ensure that your encrypted
-            data remains safe.
-          </p>
-          <p>
-            To enable two-factor authentication, please scan the QR code below
-            (or copy the secret code) into your authenticator app. We recommend
-            <a
-              class="text-blue-600"
-              href="https://getaegis.app/"
-              target="_blank"
-              >Aegis Authenticator</a
-            >
-            for Android, or
-            <a
-              class="text-blue-600"
-              href="https://www.tofuauth.com/"
-              target="_blank"
-              >Tofu</a
-            >
-            for iOS, but Google Authenticator or Authy will work fine too.
-          </p>
-
-          <hr />
-
-          <!-- QR code and secret -->
-          <img class="mx-auto" :src="totpSecretQr" alt="Secret QR Code" />
-          <pre class="text-center">{{ totpSecret }}</pre>
-
-          <hr />
-
-          <form @submit.prevent="register" class="flex flex-col items-center">
-            <p class="text-center mb-4">
-              Please enter the six-digit code from your authenticator app.
-            </p>
-            <input
-              class="w-32 border-2 border-gray-200 p-3 mb-4 rounded outline-none text-center focus:border-orange-500 transition duration-200"
-              type="number"
-              v-model="totpCode"
-              autofocus
-            />
-            <button
-              class="w-full mb-2 bg-orange-500 hover:bg-orange-400 p-4 rounded text-yellow-900 transition duration-300"
-              type="submit"
-            >
-              Submit
-            </button>
-
-            <!-- Cancel button -->
-            <button
-              class="w-full bg-gray-100 hover:bg-gray-200 p-4 rounded text-gray-900 transition duration-300"
-              @click="showTwoFactorSetup = false"
-            >
-              Cancel
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-    <div
-      v-if="showTwoFactorSetup"
-      class="absolute inset-0 z-40 opacity-50 bg-black"
-    ></div>
   </div>
 </template>
 
 <script lang="ts">
 import authService from "@/service/api/authService";
-import qrcode from "qrcode";
-import { authenticator } from "otplib";
 import zxcvbn from "zxcvbn";
 
 import { defineComponent } from "vue";
@@ -205,17 +134,13 @@ export default defineComponent({
     return {
       step: 0,
 
+      email: "",
       username: "",
       password: "",
 
       error: "",
       pwError: "",
-      debounce: null,
-
-      showTwoFactorSetup: false,
-      totpSecret: "",
-      totpSecretQr: "",
-      totpCode: "",
+      debounce: null
     };
   },
   methods: {
@@ -273,6 +198,13 @@ export default defineComponent({
         return this.$toast.error("Username cannot be empty!");
       }
 
+      // Checker if username valid
+      try {
+        await authService.CheckUsername(this.username);
+      } catch (e) {
+        return this.$toast.error(e.response.data.error);
+      }
+
       // Check if passwords are empty
       if (!this.password || !this.confirmPassword) {
         return this.$toast.error("Passwords cannot be empty!");
@@ -283,13 +215,6 @@ export default defineComponent({
         return this.$toast.error("Passwords do not match!");
       }
 
-      // Generate secret and QR code
-      const secret = authenticator.generateSecret(16);
-      this.totpSecret = secret;
-
-      const otpauth = authenticator.keyuri(this.username, "feirm.com", secret);
-      this.totpSecretQr = await qrcode.toDataURL(otpauth);
-
       // Continue to disclaimer page
       this.step++;
     },
@@ -299,10 +224,9 @@ export default defineComponent({
 
       // Bundle it into an account object
       const encryptedAccount: EncryptedAccount = {
+        email: this.email,
         username: this.username,
-        encrypted_key: key,
-        totp_secret: this.totpSecret,
-        totp_code: this.totpCode,
+        encrypted_key: key
       };
 
       // Submit account object to API

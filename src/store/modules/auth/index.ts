39ef3dd874790bf5e4950a88738ba83e68b50895
Jack Coble <jack@coble.uk>
2021-04-18 16:48:18 +0100

    Remove refresh token state from Vuex auth module

--- src/store/modules/auth/index.ts
+++ src/store/modules/auth/index.ts
@@ -1,11 +1,8 @@
-import jwt_decode from "jwt-decode";
-
 export const auth = {
   // Authentication module state
   state: {
     auth: {
       accessToken: localStorage.getItem("accessToken") || "",
-      refreshToken: localStorage.getItem("refreshToken") || ""
     }
   },
 
@@ -15,46 +12,27 @@ export const auth = {
       state.auth.accessToken = accessToken;
       localStorage.setItem("accessToken", accessToken);
     },
-    setRefreshToken(state: any, refreshToken: string) {
-      state.auth.refreshToken = refreshToken;
-      localStorage.setItem("refreshToken", refreshToken);
-    },
   },
 
   // Actions
   actions: {
-    login({ commit }, { accessToken, refreshToken }) {
+    login({ commit }, accessToken: string) {
       commit("setAccessToken", accessToken);
-      commit("setRefreshToken", refreshToken);
     }
   },
 
   // Getters
   getters: {
     getAccessToken: (state: any) => state.auth.accessToken,
-    getRefreshToken: (state: any) => state.auth.refreshToken,
 
     isLoggedIn: (state: any) => {
-      // If there is no refresh token, user is not logged in
-      const token = state.auth.refreshToken;
+      // If there is no access token, user is not logged in
+      const token = state.auth.accessToken;
       if (!token) {
         return false;
       }
 
-      // Decode the refresh token
-      const decoded: any = jwt_decode(token);
-      if (!decoded.exp) {
-        return false;
-      }
-
-      const date = new Date(0);
-      date.setUTCSeconds(decoded.exp);
-
-      if (date > new Date()) {
-        return true;
-      }
-
-      return false;
+      return true;
     }
   }
 };

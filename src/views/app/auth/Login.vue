e2bd5670cfb9ad671a5e399b1a0f8f73dc7b33f1
Jack Coble <jack@coble.uk>
2021-04-18 20:14:05 +0100

    Handle login approval requests and decryption

--- src/views/app/auth/Login.vue
+++ src/views/app/auth/Login.vue
@@ -14,7 +14,7 @@
         enter the credentials for your Feirm account.
       </p>
 
-      <form v-on:submit.prevent="submitLogin" class="space-y-3">
+      <form v-on:submit.prevent="loginRequest" class="space-y-3">
         <!-- Username -->
         <label class="block text-orange">Username</label>
         <input
@@ -34,19 +34,11 @@
           placeholder="Please enter your password"
         />
 
-        <!-- TOTP input -->
-        <label class="block text-orange">TOTP Code</label>
-        <input
-          class="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-orange-500 transition duration-200"
-          v-model="totp"
-          type="number"
-          placeholder="Two-factor authentication code"
-        />
-
         <!-- Submit button -->
         <button
           class="block w-full bg-orange-500 hover:bg-orange-400 p-4 rounded text-yellow-900 transition duration-300"
           type="submit"
+          :disabled="submitted"
         >
           <p v-if="!submitted">Log in</p>
           <img
@@ -57,10 +49,15 @@
           />
         </button>
       </form>
+
+      <!-- Check email prompt -->
+      <div v-if="checkEmail" class="bg-green-300 text-center p-3 rounded">
+        <p>Please check your email and approve the login request for your account. Keep this page open.</p>
+      </div>
     </div>
 
     <!-- Image from Unsplash -->
-    <div class="flex-auto bg-grey-500 hidden lg:contents">
+    <div class="flex-auto bg-grey-500 text-gray-900 hidden lg:contents">
       <img
         class="object-right h-full w-full"
         src="https://images.unsplash.com/photo-1592838890225-2c052fa0cf34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
@@ -85,7 +82,8 @@ export default defineComponent({
     return {
       username: "",
       password: "",
-      totp: "",
+
+      checkEmail: false,
 
       submitted: false,
       readyToDecrypt: false,
@@ -95,66 +93,80 @@ export default defineComponent({
   methods: {
     ...mapActions(["login"]),
 
-    async submitLogin() {
+    async loginRequest() {
+      // Send a login request to users email address
       this.submitted = true;
 
-      // Send details to auth API
-      try {
-        const tokens = await authService.Login(this.username, this.totp);
-        
-        const accessToken = tokens.data.access_token;
+      let tokenId: string;
 
-        // Update the access token in Vuex
-        this.login(accessToken)
+      try {
+        const res = await authService.CreateLoginRequest(this.username);
+        tokenId = res.data.id;
 
       } catch (e) {
         this.submitted = false;
         return this.$toast.error(e.response.data.error);
       }
 
-      // Attempt to decrypt the account
-      try {
-        // Fetch encrypted account payload
-        const res = await authService.GetAccount();
-        const encAccount = res.data as EncryptedAccount;
+      // Prompt user to check their email
+      this.checkEmail = true;
 
-        // Decrypt root key with password
-        const rootKey = await account.decryptRootKey(this.password, encAccount);
-        account.setRootKey(rootKey);
-      } catch (e) {
-        this.submitted = false;
-        return this.$toast.error("Failed to login, please try again.")
-      }
+      // Check the status of the token every 2 seconds
+      const interval = setInterval(async() => {
+        try {
+          const res = await authService.LoginRequestStatus(tokenId);
+          // Set the encrypted account payload if it isn't false
+          if (res.data.approved !== false) {
+            // Stop the interval
+            clearInterval(interval);
 
-      // Stop spinning and navigate to app page
-      this.submitted = false;
-      this.router.push("/app")
-    },
+            // Set account payload
+            const encryptedAccount = res.data as EncryptedAccount;
 
-    async decryptAccount() {
-      // Decrypt the root key
-      this.decrypting = true;
+            // Attempt to decrypt the root key with the password
+            try {
+              const rootKey = await account.decryptRootKey(this.password, encryptedAccount);
+              account.setRootKey(rootKey)
 
-      try {
-        // Fetch the encrypted account
-        const res = await authService.GetAccount();
-        const encryptedAccount = res.data;
+              this.checkEmail = false;
+            } catch (e) {
+                this.checkEmail = false;
+                this.submitted = false;
 
-        const rootKey = await account.decryptRootKey(
-          this.encryptionKey,
-          encryptedAccount
-        );
+                return this.$toast.error("Decryption failed, please check your password and try again.")
+            }
 
-        // We have the root key, so we can set it
-        account.setRootKey(rootKey);
-      } catch (e) {
-        this.decrypting = false;
-        return this.$toast.error("Failed to decrypt account! Please try again.");
-      }
+            // Attempt to sign and submit the login token
+            try {
+              // Get the root key
+              const rootKey = account.getRootKey()
 
-      // Push to main app route
-      this.decrypting = false;
-      this.router.push("/app");
+              // Derive identity keypair and sign the token nonce
+              const keypair = await account.deriveIdentityKeypair(rootKey);
+              const signature = await account.signMessage(keypair, encryptedAccount.token?.nonce!)
+
+              // Send to API
+              const res = await authService.CreateLoginSession(this.username, encryptedAccount.token?.id!, signature)
+              
+              // Set access token in Vuex and push to app
+              const accessToken = res.data.access_token;
+              this.login(accessToken);
+              this.router.push("/app");
+
+            } catch (e) {
+              this.submitted = false;
+              this.checkEmail = false;
+          
+              return this.$toast.error(e);
+            }
+          }
+        } catch (e) {
+          this.submitted = false;
+          this.checkEmail = false;
+
+          return this.$toast.error(e);
+        }
+      }, 2000);
     }
   },
   setup() {

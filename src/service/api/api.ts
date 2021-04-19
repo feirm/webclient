c43e6c61044a277d8c2034c07614d98bf6ce7b06
Jack Coble <jack@coble.uk>
2021-04-19 13:16:21 +0100

    Update axios interceptor to sign and submit tokens

--- src/service/api/api.ts
+++ src/service/api/api.ts
@@ -1,3 +1,5 @@
+import account from "@/class/account";
+import { SignedSessionToken } from "@/models/account";
 import { store } from "@/store";
 import axios from "axios";
 import authService from "./authService";
@@ -39,16 +41,36 @@ gatewayApi.interceptors.response.use(
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      // Fetch the existing refresh token
-      const refreshToken = store.getters.getRefreshToken;
+      // Get the root key
+      const rootKey = account.getRootKey();
+      if (!rootKey) {
+        return Promise.reject("No root key present!");
+      }
 
-      // Request for new token pair from auth API
-      const tokens = await authService.RefreshTokens(refreshToken);
-      const accessToken = tokens.data.access_token;
-      const newRefreshToken = tokens.data.refresh_token;
+      // Derive identity keypair
+      const keypair = await account.deriveIdentityKeypair(rootKey);
 
-      // Update the refresh and access tokens in Vuex
-      store.dispatch("login", { accessToken, newRefreshToken });
+      // Request for new token to be signed from auth API
+      const token = await authService.GetRefreshSessionToken();
+
+      const tokenId = token.data.id;
+      const nonce = token.data.nonce;
+
+      // Sign the nonce with the identity keypair
+      const signed = await account.signMessage(keypair, nonce);
+
+      const signedToken: SignedSessionToken = {
+        username: "",
+        token_id: tokenId,
+        signature: signed
+      }
+
+      // Submit to API
+      const res = await authService.SubmitSignedSessionToken(signedToken);
+      const accessToken = res.data.access_token;
+
+      // Update the access token in Vuex
+      store.dispatch("login", accessToken);
 
       // Update the Authorization header on the original request
       originalRequest.headers.Authorization = "Bearer " + accessToken;

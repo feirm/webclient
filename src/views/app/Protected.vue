5c8c38bd102fd16d4daf62f63d7a1789d2133b4e
Jack Coble <jack@coble.uk>
2021-04-19 13:16:32 +0100

    Update app index

--- src/views/app/Protected.vue
+++ src/views/app/Protected.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>Protected resource!</h1>
+  <div class="flex flex-col">
+
+    <!-- Wallet -->
+    <div class="p-6 md:p-12 space-y-4 w-full md:w-1/4 mx-auto">
+      <h1 class="text-5xl font-light text-center">Your wallets</h1>
+      <div class="p-4 bg-gray-200 rounded shadow">
+        <p>Store your cryptocurrency securely with our web wallet.</p>
+      </div>
+    </div>
+
   </div>
 </template>
 

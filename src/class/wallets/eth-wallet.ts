acfc27aa252b14b20cccc049d5cbbc94ff7211c2
Jack Coble <jack@coble.uk>
2021-04-30 16:30:03 +0100

    Update ETH wallet to set and get address

--- src/class/wallets/eth-wallet.ts
+++ src/class/wallets/eth-wallet.ts
@@ -1,14 +1,16 @@
 import { mnemonicToSeedSync } from "bip39";
 import { AbstractWallet } from "./abstract-wallet";
-import { hdkey } from "ethereumjs-wallet";
+import Wallet, { hdkey } from "ethereumjs-wallet";
 
 /**
  * A BIP-44 compatible wallet suitable for Ethereum and Binance Smart Chain
  * Derived from a BIP39 mnemonic
  */
 class ETHWallet extends AbstractWallet {
+    private address: string;
+
     // Derive root key etc
-    public getWallet(index: number) {
+    public getWallet(index: number): Wallet {
         // Convert mnemonic to seed
         const mnemonic = this.getMnemonic();
         const seed = mnemonicToSeedSync(mnemonic);
@@ -19,10 +21,19 @@ class ETHWallet extends AbstractWallet {
         // Derive a wallet from node
         const path = "m/44'/60'/0'/0"; // Standard BIP-44
         const node = rootKey.derivePath(path).deriveChild(index);
+
         const wallet = node.getWallet();
-        
-        console.log(wallet.getAddressString());
-        console.log(wallet.getPrivateKeyString());
+        return wallet;
+    }
+
+    // Set address
+    public setAddress(address: string) {
+        this.address = address;
+    }
+
+    // Get address
+    public getAddress(): string {
+        return this.address
     }
 }
 

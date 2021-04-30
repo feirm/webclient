a13d3aef04d4039140243655dbb9e1d4d499aaf4
Jack Coble <jack@coble.uk>
2021-04-30 17:05:26 +0100

    Method to encrypt abstract wallet

--- src/class/wallets/abstract-wallet.ts
+++ src/class/wallets/abstract-wallet.ts
@@ -1,5 +1,9 @@
+import bufferToHex from "@/helpers/bufferToHex";
+import { EncryptedWallet } from "@/models/wallet";
 import { entropyToMnemonic, validateMnemonic } from "bip39";
+import Wallet from "ethereumjs-wallet";
 import { v4 as uuidv4 } from "uuid";
+import account from "../account";
 
 export abstract class AbstractWallet {
     private id: string; // Random UUID
@@ -46,5 +50,37 @@ export abstract class AbstractWallet {
     // Return the 24-word mnemonic if we have it
     public getMnemonic(): string {
         return this.mnemonic;
+    }
+
+    // Encrypt wallet using account encryption key
+    public async encrypt(rootKey: Uint8Array): Promise<EncryptedWallet> {
+        // Derive encryption key from root key and also an IV
+        const encryptionKey = await account.deriveEncryptionKey(rootKey);
+        const iv = window.crypto.getRandomValues(new Uint8Array(16));
+
+        // Convert the mnemonic to bytes
+        const mnemonicBytes = new TextEncoder().encode(this.getMnemonic());
+
+        // Construct encrypted wallet payload
+        const wallet = {
+            id: this.getId(),
+            iv: bufferToHex(iv),
+        } as EncryptedWallet;
+
+        // Encrypt the mnemonic with AES-256-CBC
+        const ciphertext = await window.crypto.subtle.encrypt(
+            { name: "AES-CBC", iv: iv },
+            encryptionKey,
+            mnemonicBytes
+        );
+  
+        // Sign the ciphertext with our identity key
+        const keypair = await account.deriveIdentityKeypair(rootKey);
+        const signature = await account.signMessage(keypair, bufferToHex(ciphertext));
+
+        wallet.ciphertext = bufferToHex(ciphertext);
+        wallet.signature = signature;
+
+        return wallet;
     }
 }

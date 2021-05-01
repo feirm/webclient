4f18607b664bf64496acdb21f381637e98b77a74
Jack Coble <jack@coble.uk>
2021-05-01 13:12:26 +0100

    Update Vuex auth module

--- src/store/modules/auth/index.ts
+++ src/store/modules/auth/index.ts
@@ -1,3 +1,5 @@
+import account from "@/class/account";
+
 export const auth = {
   // Authentication module state
   state: {
@@ -51,6 +53,12 @@ export const auth = {
       // If there is no access token, user is not logged in
       const token = state.auth.accessToken;
       if (!token) {
+        return false;
+      }
+
+      // If a root key is not present, the user is not logged in
+      const rootKey = account.getRootKey();
+      if (!rootKey) {
         return false;
       }
 

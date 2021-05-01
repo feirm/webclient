87009b198570fee8d65605913925e0e37edddf5d
Jack Coble <jack@coble.uk>
2021-05-01 16:15:45 +0100

    Re-design of the home page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,115 +1,248 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <!-- Landing hero -->
-  <div class="flex-grow p-10 sm:p-40 bg-grey-500">
-    <div>
-      <h1
-        class="md:w-2/4 mx-auto text-4xl text-center font-light md:text-6xl mb-10 text-orange"
-      >
-        An ecosystem where privacy is for everyone.
-      </h1>
-      <p class="md:w-1/3 mx-auto text-2xl font-light text-center text-gray-100">
-        A blockchain project and platform built for your privacy. Take a deep
-        dive into the Feirm Ecosystem and experience the ultimate privacy and
-        security provided by design.
-      </p>
+    <div class="flex-grow p-10 sm:p-40 bg-grey-500">
+      <div>
+        <h1
+          class="md:w-2/4 mx-auto text-4xl text-center font-light md:text-6xl mb-10 text-orange"
+        >
+          An ecosystem where privacy is for everyone.
+        </h1>
+        <p
+          class="md:w-1/3 mx-auto text-2xl font-light text-center text-gray-100"
+        >
+          A blockchain project and platform built for your privacy. Take a deep
+          dive into the Feirm Ecosystem and experience the ultimate privacy and
+          security provided by design.
+        </p>
+      </div>
     </div>
-  </div>
 
-  <!-- Highlighted services -->
-  <div class="bg-gray-200">
-    <div class="container p-8 md:p-16 w-full md:w-3/4 mx-auto space-y-4 md:space-y-12">
-      <h1 class="text-4xl font-light text-center text-gray-900">Explore the ecosystem</h1>
-
-      <div class="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-12 text-left">
-        <!-- Multi asset wallet -->
-        <div class="p-4 bg-grey-100 space-y-2 shadow rounded">
-          <h1 class="text-2xl text-center text-orange">Multi-asset wallets</h1>
-          <p>The Feirm Platform provides you with highly secure web wallet to store your assets. All wallets are created using industry standards (BIP39 and BIP44).</p>
-          <p>You can choose to keep your wallet on your device for better security, or store an encrypted version in the cloud for greater convenience if you use multiple devices.</p>
-        </div>
-
-        <!-- Feirm token -->
-        <div class="p-4 bg-grey-100 space-y-2 shadow rounded">
-          <h1 class="text-2xl text-center text-orange">Feirm token</h1>
-          <p>The Feirm token, also known as XFE, is a utility token on the Binance Smart Chain (BEP-20). It is fully throughout the Feirm platform and services we offer.</p>
-          <p>Benefit from reduced fees, additional rewards, and much more by holding it!</p>
-
-          <p class="text-center">
-            <router-link to="/" class="text-blue-600 hover:text-blue-800 visited:text-purple-600">Learn more</router-link>
+    <!-- What do we offer? -->
+    <div class="py-12 bg-white">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+          <p
+            class="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl"
+          >
+            What do we have to offer?
           </p>
         </div>
 
-        <!-- Social media programme -->
-        <div class="p-4 bg-grey-100 space-y-2 shadow rounded">
-          <h1 class="text-2xl text-center text-orange">Engagement rewards</h1>
-          <p>Earn additional XFE on a frequent basis by participating in activities on social media.</p>
-          <p>You will be able to spread awareness of the Feirm project and earn points from each activity. These points are weighted and can be redeemed for the XFE token.</p>
-        </div>
-
-        <!-- Research -->
-        <div class="p-4 bg-grey-100 space-y-2 shadow rounded">
-          <h1 class="text-2xl text-center text-orange">Scientific research</h1>
-          <p>Have some spare computing power to hand?</p>
-          <p>Put it towards the World Community Grid and help power cutting-edge scientific research.</p>
-          <p>This is another great way to earn the XFE token, and is fully integrated into the platform.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Features container -->
-  <div class="bg-grey-100">
-    <div class="container p-8 md:p-16 w-full md:w-3/4 mx-auto space-y-4 md:space-y-12">
-      <h1 class="text-4xl font-light text-center text-gray-900">What else do we have to offer?</h1>
-
-      <!-- Feature grids -->
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-12 text-left">
-        <div class="p-4 space-y-2">
-          <svg class="w-6 h-6 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
-          <h1 class="text-2xl font-light text-center text-orange">Non-custodial web wallet</h1>
-          <p>Not your keys, not your coins. With Feirm and our non-custodial wallet, you remain in complete control of your cryptocurrency.</p>
-        </div>
-
-        <div class="p-4 space-y-2">
-          <svg class="w-6 h-6 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
-          <h1 class="text-2xl font-light text-center text-orange">Ultimate security</h1>
-          <p>When you create a Feirm account, it is protected with two-factor authentication and heavily secured using zero-knowledge encryption.</p>
-        </div>
-
-        <div class="p-4 space-y-2">
-          <svg class="w-6 h-6 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-          <h1 class="text-2xl font-light text-center text-orange">Available for everyone</h1>
-          <p>We really do want privacy to be available for everyone. The Feirm Platform can be accessed on any device with a web browser - no need to install smartphone apps!</p>
-        </div>
-
-        <div class="p-4 space-y-2">
-          <svg class="w-6 h-6 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-          <h1 class="text-2xl font-light text-center text-orange">Open source</h1>
-          <p>Trust is important, but never good enough. The Feirm web client is open source for everyone to see and audit its functionality.</p>
-        </div>
-
-        <div class="p-4 space-y-2">
-          <svg class="w-6 h-6 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path></svg>
-          <h1 class="text-2xl font-light text-center text-orange">No ads or trackers</h1>
-          <p>We aren't in the business of serving advertisements or violating user privacy by tracking your every move.</p>
-        </div>
-
-        <div class="p-4 space-y-2">
-          <svg class="w-6 h-6 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"></path></svg>
-          <h1 class="text-2xl font-light text-center text-orange">Anonymity</h1>
-          <p>Everyone deserves the right to remain anonymous online. All you need to get started is a username. No need for email addresses or phone numbers.</p>
+        <div class="mt-10">
+          <dl
+            class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10"
+          >
+            <div
+              v-for="feature in features"
+              :key="feature.name"
+              class="relative"
+            >
+              <dt>
+                <div
+                  class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-orange-600 to-yellow-500 text-white"
+                >
+                  <component
+                    :is="feature.icon"
+                    class="h-6 w-6"
+                    aria-hidden="true"
+                  />
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  {{ feature.name }}
+                </p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                {{ feature.description }}
+              </dd>
+            </div>
+          </dl>
         </div>
       </div>
     </div>
-  </div>
+
+    <!-- Features -->
+    <div class="relative bg-grey-100 py-16">
+      <div
+        class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl"
+      >
+        <h2
+          class="text-base font-semibold tracking-wider text-orange uppercase"
+        >
+          Features
+        </h2>
+        <p
+          class="mt-2 text-3xl font-bold text-gray-900 tracking-tight sm:text-4xl"
+        >
+          Explore our ecosystem.
+        </p>
+        <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+          Feirm provides a variety of applications within our ecosystem for
+          everyone to use.
+        </p>
+        <div class="mt-12">
+          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <div v-for="feature in ecosystem" :key="feature.name" class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span
+                      class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-orange-600 to-yellow-500 rounded-md shadow-lg"
+                    >
+                      <component
+                        :is="feature.icon"
+                        class="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </div>
+                  <h3
+                    class="mt-8 text-lg font-medium text-gray-900 tracking-tight"
+                  >
+                    {{ feature.name }}
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500 text-left">
+                    {{ feature.description }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Blog posts -->
+    <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pb-28 lg:px-8">
+      <div class="absolute inset-0">
+        <div class="bg-white h-1/3 sm:h-2/3" />
+      </div>
+      <div class="relative max-w-7xl mx-auto">
+        <div class="text-center">
+          <h2
+            class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"
+          >
+            Latest articles
+          </h2>
+          <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+            Take a look at what we've been getting up to recently!
+          </p>
+        </div>
+        <div
+          class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none"
+        >
+          <div
+            v-for="post in articles"
+            :key="post.title"
+            class="flex flex-col rounded-lg shadow-lg overflow-hidden"
+          >
+            <div class="flex-shrink-0">
+              <img class="h-48 w-full object-cover" :src="post.thumbnail" />
+            </div>
+            <div
+              class="flex-1 bg-white p-6 flex flex-col justify-between space-y-3"
+            >
+              <div class="flex-1">
+                <a :href="post.link" class="block mt-2">
+                  <p class="text-xl text-center font-semibold text-gray-900">
+                    {{ post.title }}
+                  </p>
+                </a>
+              </div>
+              <div class="flex items-center mx-auto">
+                <div class="flex space-x-1 text-sm text-gray-500">
+                  <time :datetime="post.pubDate">
+                    {{ post.pubDate }}
+                  </time>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import {
+  GlobeIcon,
+  HeartIcon,
+  FingerPrintIcon,
+  CurrencyPoundIcon,
+  CollectionIcon,
+  SwitchHorizontalIcon,
+} from "@heroicons/vue/outline";
+import axios from "axios";
+
+const features = [
+  {
+    name: "Ultimate security",
+    description:
+      "Feirm accounts are protected with mandatory two-factor authentication, and heavily encrypted on your device with zero-knowledge encryption before being sent to our servers.",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Web wallet",
+    description:
+      "Feirm provides a non-custodial wallet featuring support for many cryptocurrencies and tokens. Link an encrypted version of your wallet to your Feirm account, or keep your wallet local to your device.",
+    icon: CurrencyPoundIcon,
+  },
+  {
+    name: "Open source",
+    description:
+      "Trust is important when it comes to dealing with cryptography, but it isn't enough. The Feirm web-client is open source for everyone to see.",
+    icon: HeartIcon,
+  },
+  {
+    name: "Available for everyone",
+    description:
+      "Privacy should be accessible for everyone. Your Feirm account can be accessed on any device with a web browser - no need for more smartphone apps!",
+    icon: GlobeIcon,
+  }
+];
+
+const ecosystem = [
+  {
+    name: "Feirm token",
+    description:
+      "Feirm has its own utility token called XFE currently operating on the Binance Smart Chain. It is the backbone of the Feirm ecosystem.",
+    icon: CollectionIcon,
+  },
+  {
+    name: "Bridge",
+    description:
+      "Migrate your old native XFE coins to the new token through our bridge. The process is entirely automated and instant!",
+    icon: SwitchHorizontalIcon,
+  },
+  {
+    name: "Lorem ipsum",
+    description: "Lorem ipsum loreum ipsum",
+    icon: CollectionIcon,
+  }
+];
 
 export default defineComponent({
-  name: "Index"
+  name: "Index",
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  async mounted() {
+    const MEDIUM_URL = "https://medium.com/feed/feirm";
+    const RSS_JSON =
+      "https://api.rss2json.com/v1/api.json?rss_url=" + MEDIUM_URL;
+
+    const articles = await axios.get(RSS_JSON);
+    this.articles = articles.data.items.splice(0, 3);
+  },
+  setup() {
+    return {
+      features,
+      ecosystem,
+    };
+  },
 });
 </script>

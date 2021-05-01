a40d44efeec9a6016702e3f48593791eef2d3066
Jack Coble <jack@coble.uk>
2021-05-01 13:10:53 +0100

    Start of new navigation component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,103 +1,108 @@
 <template>
-  <!-- navbar goes here -->
-  <nav class="bg-grey-500 sticky">
-    <div class="max-w-7xl mx-auto px-4">
-      <div class="flex justify-between">
-        <div class="flex space-x-4">
-          <!-- Logo showcase -->
-          <div>
-            <router-link
-              :to="isLoggedIn ? '/app' : '/'"
-              class="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
-            >
-              <img
-                class="object-scale-down w-36"
-                src="@/assets/img/navbar_logo.webp"
-                alt="Navbar Logo"
-              />
-            </router-link>
+  <Disclosure as="nav" class="bg-white shadow" v-slot="{ open }">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between h-16">
+        <div class="flex">
+          <div class="flex-shrink-0 flex items-center">
+            <img class="block lg:hidden h-10 w-auto" src="@/assets/img/logo.webp" alt="Feirm" />
+            <img class="hidden lg:block h-10 w-auto" src="@/assets/img/logo.webp" alt="Feirm" />
           </div>
-
-          <!-- Left navigation -->
-          <div class="hidden md:flex items-center space-x-1">
-            <a href="#" class="py-5 px-3 text-white hover:text-gray-300"
-              >Platform</a
-            >
-            <a href="#" class="py-5 px-3 text-white hover:text-gray-300"
-              >Blockchain</a
-            >
-            <router-link
-              to="/bridge"
-              class="py-5 px-3 text-white hover:text-gray-300"
-              >Bridge</router-link
-            >
+          <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
+            <a href="#" class="border-orange-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+              Wallet
+            </a>
           </div>
         </div>
+        <div class="hidden sm:ml-6 sm:flex sm:items-center">
+          <!-- Profile dropdown -->
+          <Menu as="div" class="ml-3 relative">
+            <div>
+              <MenuButton class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+                <span class="sr-only">Open user menu</span>
+                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              </MenuButton>
+            </div>
+            <transition enter-active-class="transition ease-out duration-200" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+              <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <!-- My account -->
+                <router-link custom to="/app/account" v-slot="{ href, navigate }">
+                  <MenuItem :href="href" @click="navigate" v-slot="{ active }" >
+                    <a :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">My account</a>
+                  </MenuItem>
+                </router-link>
 
-        <!-- Right navigation -->
-        <div class="hidden md:flex items-center space-x-1">
-          <router-link
-            v-if="!isLoggedIn"
-            to="/app/login"
-            class="py-2 mr-3 px-5 bg-gray-50 hover:bg-gray-200 rounded transition duration-300"
-            >Login</router-link
-          >
-          <router-link
-            v-if="!isLoggedIn"
-            to="/app/signup"
-            class="py-2 px-3 bg-orange-500 hover:bg-orange-400 text-yellow-900 hover:text-yellow-800 rounded transition duration-300"
-            >Create an account</router-link
-          >
-
-          <router-link v-if="isLoggedIn" to="/app/account" class="text-gray-100 pl-3">{{ getUsername }}</router-link>
+                <!-- TODO: Add more links -->
+              </MenuItems>
+            </transition>
+          </Menu>
         </div>
-
-        <!-- Mobile burger button -->
-        <div class="md:hidden flex items-center">
-          <button class="mobile-menu-button">
-            <svg
-              class="w-6 h-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
+        <div class="-mr-2 flex items-center sm:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
         </div>
       </div>
     </div>
 
-    <!-- Mobile navigatioon -->
-    <div class="mobile-menu hidden md:hidden">
-      <router-link
-        to="/platform"
-        class="block py-2 px-4 text-sm hover:bg-gray-200"
-        >Platform</router-link
-      >
-      <router-link
-        to="/blockchain"
-        class="block py-2 px-4 text-sm hover:bg-gray-200"
-        >Blockchain</router-link
-      >
-    </div>
-  </nav>
+    <DisclosurePanel class="sm:hidden">
+      <div class="pt-2 pb-3 space-y-1">
+        <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
+        <a href="#" class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Dashboard</a>
+        <a href="#" class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Team</a>
+        <a href="#" class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Projects</a>
+        <a href="#" class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Calendar</a>
+      </div>
+      <div class="pt-4 pb-3 border-t border-gray-200">
+        <div class="flex items-center px-4">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium text-gray-800">Tom Cook</div>
+            <div class="text-sm font-medium text-gray-500">tom@example.com</div>
+          </div>
+          <button class="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <span class="sr-only">View notifications</span>
+            <BellIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-3 space-y-1">
+          <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">My account</a>
+          <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Settings</a>
+          <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Sign out</a>
+        </div>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "@vue/runtime-core";
-import { mapGetters } from "vuex";
+<script>
+import { ref } from 'vue'
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
 
-export default defineComponent({
-  name: "Navigation",
-  computed: {
-    ...mapGetters(["isLoggedIn", "getUsername"])
-  }
-});
+export default {
+  components: {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    BellIcon,
+    MenuIcon,
+    XIcon,
+  },
+  setup() {
+    const open = ref(false)
+
+    return {
+      open,
+    }
+  },
+}
 </script>

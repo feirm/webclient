e0f6a74a204590a68415976ca5cb3eb278833eb1
Jack Coble <jack@coble.uk>
2021-05-04 19:20:32 +0100

    Fetch and decrypt wallet

--- src/views/app/wallet/Index.vue
+++ src/views/app/wallet/Index.vue
@@ -9,21 +9,36 @@
     </header>
     <main>
       <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        
+        {{ mnemonic }}
       </div>
     </main>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import ethWallet from '@/class/wallets/eth-wallet'
+import account from '@/class/account'
 
 export default {
   setup() {
-    const open = ref(false)
+    const open = ref(false);
+    const mnemonic = ref();
+
+    onMounted(async () => {
+      // Check for encrypted wallet in IDB
+      const wallet = await ethWallet.getFromDisk();
+
+      // TODO: Check wallet microservice
+
+      // Decrypt wallet and set mnemonic
+      const rootKey = account.getRootKey();
+      mnemonic.value = await ethWallet.decryptWallet(rootKey, wallet);
+    })
 
     return {
       open,
+      mnemonic
     }
   },
 }

c3321e07b5eda9b01985180178fc3704a706e8ca
Jack Coble <jack@coble.uk>
2021-05-04 18:46:15 +0100

    IndexedDB store

---
+++ src/class/db.ts
@@ -0,0 +1,17 @@
+import { EncryptedWallet } from "@/models/wallet";
+import Dexie from "dexie";
+
+export class DB extends Dexie {
+    // Tables
+    wallets: Dexie.Table<EncryptedWallet>;
+
+    constructor() {
+        super("feirm");
+
+        this.version(1).stores({
+            wallets: 'id'
+        })
+
+        this.wallets = this.table("wallets");
+    }
+}

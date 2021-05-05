3a49055209ec097aeef63ae1844e2a1e834c96d4
Jack Coble <jack@coble.uk>
2021-05-05 19:41:30 +0100

    Set amount input field to use text type

--- src/views/app/wallet/Wallet.vue
+++ src/views/app/wallet/Wallet.vue
@@ -24,7 +24,7 @@
                     <div>
                         <label for="amount" class="block text-sm font-medium text-gray-700">Amount</label>
                         <div class="mt-1">
-                            <input name="amount" v-model="amount" type="number" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm" />
+                            <input name="amount" v-model="amount" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm" />
                         </div>
                     </div>
 

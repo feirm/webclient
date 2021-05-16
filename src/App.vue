79eb6a7be63b48794dc6e77ca98f62958922ddba
Jack Coble <jack@coble.uk>
2021-05-16 15:38:41 +0100

    Display loading spinner when clicking update prompt

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,31 @@
   <div
     v-if="isRefresh"
     @click="update"
-    class="fixed bottom-0 right-0 m-8 w-5/6 md:w-full max-w-sm"
+    class="fixed bottom-0 right-0 m-8 w-5/8 md:w-full max-w-sm"
   >
     <div
-      class="cursor-pointer w-full p-2 bg-green-200 rounded shadow-lg text-white"
+      class="cursor-pointer w-full p-3 bg-green-200 rounded shadow-lg text-white"
     >
-      <h2 class="text-2xl font-light mb-2">Update available! 🥳</h2>
-      <p>
-        There is an update available. Press here to reload automatically and
-        install it.
+      <h2 class="text-2xl font-light mb-2 text-gray-900 text-center">
+        Update available! 🥳
+      </h2>
+      <p v-if="!refreshing" class="text-gray-900">
+        A new version of the Feirm Platform is now available. Please tap or
+        click here to update.
       </p>
+      <span v-if="refreshing">
+        <svg
+          class="animate-spin h-12 w-12 text-white mx-auto"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+      </span>
     </div>
   </div>
 </template>

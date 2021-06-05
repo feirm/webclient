fed0fdc23f789d7dd39dc8e71c9a2eeb0c6fd15d
Jack Coble <jack@coble.uk>
2021-06-05 15:50:10 +0100

    Add v2 wallet model

--- src/models/wallet.ts
+++ src/models/wallet.ts
@@ -1,12 +1,27 @@
 // Representation of an encrypted wallet payload
 interface EncryptedWallet {
   id: string;
-  address: string;
   uid?: string;
   ciphertext: string;
   iv: string;
   signature: string;
   version: number;
+}
+
+// V2
+interface EncryptedWallet {
+  id: string;
+  uid?: string;
+  coins: Coins[];
+  ciphertext: string;
+  iv: string;
+  signature: string;
+  version: number;
+}
+
+interface Coins {
+  ticker: string;
+  address: string;
 }
 
 export { EncryptedWallet };

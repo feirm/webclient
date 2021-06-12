3eea57db32291811c16a8fd466a0e0acf112a2da
Jack Coble <jack@coble.uk>
2021-06-12 17:47:57 +0100

    BTC wallet method to lookup XPUB info

--- src/class/wallets/btc-abstract-wallet.ts
+++ src/class/wallets/btc-abstract-wallet.ts
@@ -3,16 +3,30 @@ import { CoinFactory } from "../coins";
 import { AbstractWallet } from "./abstract-wallet";
 
 abstract class BTCWallet extends AbstractWallet {
+  // Create an Axios instance for Blockbook API
   createBlockbookClient(ticker: string): AxiosInstance {
     const coin = CoinFactory.getCoin(ticker);
     const blockbookUrl = coin.blockbook;
 
     if (blockbookUrl) {
       const blockbookClient = axios.create({
-        baseURL: blockbookUrl,
+        baseURL: `${blockbookUrl}/api/v2/`,
       });
 
       return blockbookClient;
+    }
+  }
+
+  // Lookup details for an XPUB/YPUB/ZPUB
+  async getXpubInfo(xpub, ticker: string) {
+    const blockbook = this.createBlockbookClient(ticker);
+
+    try {
+      await blockbook.post(`xpub/${xpub}`).then((res) => {
+        console.log(res.data);
+      });
+    } catch (e) {
+      throw new Error(e);
     }
   }
 }

e6359d887871084ccc90a762d967c8e55c5b5066
Jack Coble <jack@coble.uk>
2021-06-14 12:25:14 +0100

    Trigger send modal and add icons to buttons

--- src/views/app/wallet/Wallet.vue
+++ src/views/app/wallet/Wallet.vue
@@ -8,18 +8,22 @@
           My {{ coin.name }} Wallet
         </h2>
       </div>
-      <div class="mt-4 flex md:mt-0 md:ml-4">
+      <div class="mt-4 flex md:mt-0 md:ml-4 space-x-3">
         <button
           @click="toggleReceivingAddressModal"
           type="button"
-          class="w-24 items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+          class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-gray-700 bg-white border-gray-300 hover:bg-gray-100"
         >
+          <QrcodeIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
           Receive
         </button>
+
         <button
+          @click="toggleSendModal"
           type="button"
-          class="w-24 ml-3 items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-500 hover:bg-orange-400"
+          class="inline-flex items-center px-4 py-2 shadow-sm text-base font-medium rounded-md text-white bg-orange-500 hover:bg-orange-400"
         >
+          <ArrowCircleRightIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
           Send
         </button>
       </div>
@@ -34,6 +38,8 @@
     :address="address"
     @close="toggleReceivingAddressModal"
   />
+
+  <send-modal v-if="showSendModal" :ticker="ticker" @close="toggleSendModal" />
 </template>
 
 <script lang="ts">
@@ -43,6 +49,9 @@ import { Coin, CoinFactory } from "@/class/coins";
 
 // Components
 import AddressModal from "@/components/AddressModal.vue";
+import SendModal from "@/components/SendModal.vue";
+
+// Other
 import walletService from "@/service/api/walletService";
 import account from "@/class/account";
 import ethWallet from "@/class/wallets/eth-wallet";
@@ -51,10 +60,16 @@ import btcP2wpkhWallet from "@/class/wallets/btc-p2wpkh-wallet";
 import sb from "satoshi-bitcoin";
 import Web3 from "web3";
 
+import { QrcodeIcon, ArrowCircleRightIcon } from "@heroicons/vue/outline";
+
 export default defineComponent({
   name: "Wallet",
   components: {
     AddressModal,
+    SendModal,
+
+    QrcodeIcon,
+    ArrowCircleRightIcon,
   },
   setup() {
     const route = useRoute();
@@ -66,6 +81,7 @@ export default defineComponent({
     const address = ref();
 
     const showAddressModal = ref(false);
+    const showSendModal = ref(true);
 
     onMounted(async () => {
       coin.value = CoinFactory.getCoin(ticker);
@@ -128,13 +144,22 @@ export default defineComponent({
       showAddressModal.value = !showAddressModal.value;
     };
 
+    // Toggle send modal
+    const toggleSendModal = () => {
+      showSendModal.value = !showSendModal.value;
+    };
+
     return {
       coin,
       ticker,
       address,
       balance,
+
       showAddressModal,
+      showSendModal,
+
       toggleReceivingAddressModal,
+      toggleSendModal,
     };
   },
 });

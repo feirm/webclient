db5d29b77a540f95595eb922894846fcdb38bfbc
Jack Coble <jack@coble.uk>
2021-06-15 16:31:19 +0100

    Loading skeleton on send modal

--- src/components/SendModal.vue
+++ src/components/SendModal.vue
@@ -42,10 +42,33 @@
           <div
             class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
           >
-            <div v-if="isLoaded">
+            <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+              <button
+                type="button"
+                class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
+                @click="open = false"
+              >
+                <span class="sr-only">Close</span>
+                <XIcon class="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div>
               <div class="text-center space-y-3 mb-3">
-                <img :src="coin.logo" class="w-12 mx-auto" :alt="coin.ticker" />
+                <div v-if="!isLoaded" class="animate-pulse">
+                  <div class="rounded-full bg-gray-300 h-14 w-14 mx-auto"></div>
+                </div>
+                <img
+                  v-else
+                  :src="coin.logo"
+                  class="w-12 mx-auto"
+                  :alt="coin.ticker"
+                />
+
+                <div v-if="!isLoaded" class="animate-pulse">
+                  <div class="bg-gray-300 w-1/3 h-8 mx-auto rounded"></div>
+                </div>
                 <DialogTitle
+                  v-else
                   as="h3"
                   class="text-lg leading-6 font-medium text-gray-900"
                 >
@@ -54,70 +77,103 @@
 
                 <div class="text-left space-y-2">
                   <!-- Recipient -->
-                  <label
-                    for="address"
-                    class="block text-sm font-medium text-gray-700"
-                    >Recipient Address</label
-                  >
-                  <div class="mt-1">
-                    <input
-                      type="text"
-                      class="shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
-                    />
+                  <div v-if="!isLoaded" class="animate-pulse space-y-2">
+                    <div class="bg-gray-300 w-1/3 h-6 rounded"></div>
+                    <div class="bg-gray-300 h-8 rounded"></div>
+                  </div>
+                  <div v-else>
+                    <label
+                      for="address"
+                      class="block text-sm font-medium text-gray-700"
+                      >Recipient Address</label
+                    >
+                    <div class="mt-1">
+                      <input
+                        type="text"
+                        class="shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
                   </div>
 
                   <!-- Amount -->
-                  <label
-                    for="amount"
-                    class="block text-sm font-medium text-gray-700"
-                    >Amount ({{ props.ticker.toUpperCase() }})</label
-                  >
-                  <div class="mt-1">
-                    <input
-                      type="number"
-                      class="shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
-                    />
+                  <div v-if="!isLoaded" class="animate-pulse space-y-2">
+                    <div class="bg-gray-300 w-1/3 h-6 rounded"></div>
+                    <div class="bg-gray-300 h-8 rounded"></div>
                   </div>
-
-                  <!-- Gas Limit -->
-                  <div v-if="isEth">
+                  <div v-else>
                     <label
-                      for="gaslimit"
+                      for="amount"
                       class="block text-sm font-medium text-gray-700"
-                      >Gas Limit</label
+                      >Amount ({{ props.ticker.toUpperCase() }})</label
                     >
                     <div class="mt-1">
                       <input
                         type="number"
                         class="shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
-                        :value="coin.contract ? 100000 : 21000"
                       />
                     </div>
                   </div>
 
-                  <!-- Transaction fee -->
-                  <label
-                    for="address"
-                    class="block text-sm font-medium text-gray-700"
-                    >Transaction Fee</label
-                  >
-                  <div class="flex mt-1 space-x-3 justify-center">
-                    <button
-                      v-for="fee in fees"
-                      :key="fee"
-                      class="block w-full p-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:border-orange-500"
-                    >
-                      {{ fee.speed }}
-                      <br />
-                      <span class="text-sm"
-                        >{{ fee.amount }} {{ fee.format }}</span
+                  <!-- Gas Limit -->
+                  <div v-if="isEth">
+                    <div v-if="!isLoaded" class="animate-pulse space-y-2">
+                      <div class="bg-gray-300 w-1/3 h-6 rounded"></div>
+                      <div class="bg-gray-300 h-8 rounded"></div>
+                    </div>
+                    <div v-else>
+                      <label
+                        for="gaslimit"
+                        class="block text-sm font-medium text-gray-700"
+                        >Gas Limit</label
                       >
-                    </button>
+                      <div class="mt-1">
+                        <input
+                          type="number"
+                          class="shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
+                          :value="coin.contract ? 100000 : 21000"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- Transaction fee -->
+                  <div v-if="!isLoaded" class="animate-pulse space-y-2">
+                    <div class="bg-gray-300 w-1/3 h-6 rounded"></div>
+                    <div class="flex space-x-3 justify-center">
+                      <div class="rounded bg-gray-300 h-16 w-full"></div>
+                      <div class="rounded bg-gray-300 h-16 w-full"></div>
+                      <div class="rounded bg-gray-300 h-16 w-full"></div>
+                    </div>
+                  </div>
+                  <div v-else>
+                    <label
+                      for="address"
+                      class="block text-sm font-medium text-gray-700"
+                      >Transaction Fee</label
+                    >
+                    <div class="flex mt-1 space-x-3 justify-center">
+                      <button
+                        v-for="fee in fees"
+                        :key="fee"
+                        class="block w-full p-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:border-orange-500"
+                      >
+                        {{ fee.speed }}
+                        <br />
+                        <span class="text-sm"
+                          >{{ fee.amount }} {{ fee.format }}</span
+                        >
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="space-y-2">
+
+            <div v-if="!isLoaded" class="animate-pulse space-y-2">
+              <div class="rounded bg-gray-300 h-8 w-full"></div>
+              <div class="rounded bg-gray-300 h-8 w-full"></div>
+            </div>
+            <div v-else class="space-y-2">
               <button
                 type="button"
                 class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-500 text-base font-medium text-white hover:bg-orange-400 sm:text-sm"
@@ -153,6 +209,7 @@ import { Coin, CoinFactory } from "@/class/coins";
 import axios from "axios";
 import ethWallet from "@/class/wallets/eth-wallet";
 import Web3 from "web3";
+import { XIcon } from "@heroicons/vue/outline";
 
 /*
 This component should take in an address for a prop and showcase it, a QR code, and copy to clipboard button
@@ -168,6 +225,7 @@ export default {
     DialogOverlay,
     DialogTitle,
     TransitionRoot,
+    XIcon,
   },
   setup(props, { emit }) {
     const open = ref(true);

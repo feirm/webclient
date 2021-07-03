97e00f835adb648402b22b0e735561ef58ea2ecd
Jack Coble <jack@coble.uk>
2021-07-03 18:22:36 +0100

    Start of component to view recovery codes

---
+++ src/components/account/TwoFactorRecoveryCodes.vue
@@ -0,0 +1,16 @@
+<template>
+  {{ codes }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    codes: String,
+  },
+  setup(props) {
+    console.log(props.codes);
+  },
+});
+</script>

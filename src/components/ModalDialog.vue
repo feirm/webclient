6f995b704fea5833181377ac4e69a2c138f0801b
Jack Coble <jack@coble.uk>
2021-07-04 12:58:20 +0100

    Refactor base modal component

--- src/components/ModalDialog.vue
+++ src/components/ModalDialog.vue
@@ -9,34 +9,65 @@
       leave-to-class="opacity-0"
     >
       <div
-        ref="modal-backdrop"
-        class="fixed z-10 inset-0 overflow-y-auto bg-black bg-opacity-50"
+        class="fixed z-10 inset-0 overflow-y-auto"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
         v-show="showModal"
-        @click="closeModal"
       >
-        <transition
-          enter-active-class="transition ease-out duration-300 transform "
-          enter-from-class="opacity-0 translate-y-10 scale-95"
-          enter-to-class="opacity-100 translate-y-0 scale-100"
-          leave-active-class="ease-in duration-200"
-          leave-from-class="opacity-100 translate-y-0 scale-100"
-          leave-to-class="opacity-0 translate-y-10 translate-y-0 scale-95"
+        <div
+          class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
         >
           <div
-            class="flex items-start justify-center min-h-screen pt-24 text-center"
+            @click="closeModal"
+            class="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity"
+            aria-hidden="true"
+          ></div>
+
+          <!-- This element is to trick the browser into centering the modal contents. -->
+          <span
+            class="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+            >&#8203;</span
           >
-            <div
-              class="bg-white rounded-lg text-left overflow-hidden shadow-xl p-8 w-1/3"
-              role="dialog"
-              ref="modal"
-              aria-modal="true"
-              aria-labelledby="modal-headline"
-              v-show="showModal"
-            >
-              <slot />
+
+          <div
+            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+          >
+            <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+              <button
+                @click="closeModal"
+                type="button"
+                class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
+              >
+                <span class="sr-only">Close</span>
+                <XIcon class="w-6 h-6" />
+              </button>
+            </div>
+            <div class="sm:flex sm:items-start">
+              <div class="mt-3 text-center sm:mt-0 sm:text-left">
+                <slot />
+              </div>
+            </div>
+
+            <!-- Footer -->
+            <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-500 text-base font-medium text-white hover:bg-orange-400 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                OK
+              </button>
+              <button
+                @click="closeModal"
+                type="button"
+                class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none sm:mt-0 sm:w-auto sm:text-sm"
+              >
+                Cancel
+              </button>
             </div>
           </div>
-        </transition>
+        </div>
       </div>
     </transition>
   </teleport>
@@ -44,6 +75,7 @@
 
 <script lang="ts">
 import { defineComponent, ref, watch } from "vue";
+import { XIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
   name: "ModalDialog",
@@ -52,6 +84,9 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
+  },
+  components: {
+    XIcon,
   },
   setup(props, { emit }) {
     const showModal = ref(false);

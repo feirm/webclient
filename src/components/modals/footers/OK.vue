1f786f50b17161a5fa922791e75b878aa0ef688b
Jack Coble <jack@coble.uk>
2021-07-04 17:45:54 +0100

    Update modal footers

--- src/components/modals/footers/OK.vue
+++ src/components/modals/footers/OK.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
     <button
-      @click="$emit('ok')"
+      @click.prevent="ok"
       type="button"
       class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-500 text-base font-medium text-white hover:bg-orange-400 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
     >
@@ -9,7 +9,7 @@
     </button>
 
     <button
-      @click="$emit('cancel')"
+      @click.prevent="cancel"
       type="button"
       class="w-full inline-flex justify-center rounded-md border shadow-sm px-4 py-2 text-base font-medium text-gray-900 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
     >
@@ -17,3 +17,24 @@
     </button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup(props, { emit }) {
+    const ok = () => {
+      emit("ok");
+    };
+
+    const cancel = () => {
+      emit("cancel");
+    };
+
+    return {
+      ok,
+      cancel,
+    };
+  },
+});
+</script>

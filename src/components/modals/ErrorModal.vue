9559dc6c91406e05036984613013b1069f9aaab2
Jack Coble <jack@coble.uk>
2021-07-04 17:46:05 +0100

    New error modal component

---
+++ src/components/modals/ErrorModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseModal :show="show" dismissFooter @dismiss="$emit('dismiss')">
+    <div class="space-y-3">
+      <h1 class="text-2xl">Error! 😟</h1>
+      <p class="text-sm">
+        {{ error }}
+      </p>
+    </div>
+  </BaseModal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BaseModal from "@/components/modals/BaseModal.vue";
+
+export default defineComponent({
+  name: "ErrorModal",
+  components: {
+    BaseModal,
+  },
+  props: {
+    show: {
+      type: Boolean,
+    },
+    error: {
+      type: String,
+    },
+  },
+});
+</script>

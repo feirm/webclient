6f4a188d0847c28d06aba6881bdc9742152bc7b3
Jack Coble <jack@coble.uk>
2021-07-05 17:12:08 +0100

    Remember account key options section

--- src/views/app/account/Options.vue
+++ src/views/app/account/Options.vue
@@ -9,16 +9,41 @@
         Make some changes which affect the way the Feirm Platform runs on your
         device.
       </p>
+
+      <hr />
+
+      <!-- Remember account encrpyption key -->
+      <div class="space-y-2">
+        <h2 class="text-xl">Remember Account Key</h2>
+        <p class="text-sm">
+          Your Feirm account key is a key used to encrypt your data (such as
+          wallet) on the platform. Only you have access to this key as it
+          remains in your browser. Letting your browser remember this key allows
+          you to remain logged in to your Feirm account on this device.
+        </p>
+
+        <button
+          type="button"
+          class="rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-500 text-base font-medium text-yellow-900 hover:bg-orange-400 sm:w-auto sm:text-sm"
+        >
+          Remember Key
+        </button>
+      </div>
+
+      <BaseModal />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
+import BaseModal from "@/components/modals/BaseModal.vue";
 import SideNav from "@/components/account/SideNav.vue";
 
 export default defineComponent({
   components: {
+    BaseModal,
     SideNav,
   },
 });

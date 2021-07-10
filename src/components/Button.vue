0314a4224f7cdc2c0c26788be8f4bcc397afa5bc
Jack Coble <jack@coble.uk>
2021-07-10 17:35:39 +0100

    Add light property to button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,7 +2,12 @@
   <button
     @click="onClick"
     :disabled="loading"
-    class="transition duration-300 ease-in-out px-3 py-2 w-36 text-sm border rounded-md focus:outline-none text-yellow-900 bg-orange-500 border-orange-500 hover:bg-gray-100 hover:text-gray-900 hover:border-gray-200 disabled:opacity-50"
+    class="transition duration-300 ease-in-out px-3 py-2 w-36 text-sm border rounded-md focus:outline-none disabled:opacity-50"
+    :class="
+      isLight
+        ? 'bg-gray-50 border hover:bg-gray-200'
+        : 'text-yellow-900 bg-orange-500 border-orange-500 hover:bg-gray-100 hover:text-gray-900 hover:border-gray-200'
+    "
   >
     <slot v-if="!loading">Button</slot>
     <img v-else class="mx-auto w-5" src="@/assets/loading_spinner.svg" />
@@ -20,6 +25,10 @@ export default defineComponent({
       required: false,
     },
     loading: {
+      type: Boolean,
+      default: false,
+    },
+    isLight: {
       type: Boolean,
       default: false,
     },

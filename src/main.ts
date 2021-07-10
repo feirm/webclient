c449c69a5617fb2cc7b472aec11ae8a1a8936f89
Jack Coble <jack@coble.uk>
2021-07-10 16:31:26 +0100

    Global button component (b-button)

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,14 @@ import Toaster from "@meforma/vue-toaster";
 // CSS Styles (Tailwind)
 import "@/css/index.css";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(Toaster, { position: "bottom" })
-  .mount("#app");
+// Custom components
+import Button from "@/components/Button.vue";
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(Toaster, { position: "bottom" });
+
+app.component("b-button", Button);
+
+app.mount("#app");

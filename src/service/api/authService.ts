4079a83e3cc063bc9dc7c054f9f635d9a49f5558
Jack Coble <jack@coble.uk>
2021-12-05 16:14:33 +0000

    Update endpoints for auth service

--- src/service/api/authService.ts
+++ src/service/api/authService.ts
@@ -4,28 +4,28 @@ import { EncryptedAccount, SignedSessionToken } from "@/models/account";
 export default {
   // Check username availability
   CheckUsername(username: string) {
-    return gatewayApi.post("auth/v1/check-username", {
+    return gatewayApi.post("/v1/auth/check-username", {
       username,
     });
   },
 
   // Check email availability
   CheckEmail(email: string) {
-    return gatewayApi.post("auth/v1/check-email", {
+    return gatewayApi.post("/v1/auth/check-email", {
       email,
     });
   },
 
   // Create a login request
   CreateLoginRequest(username: string) {
-    return gatewayApi.post("auth/v1/login", {
+    return gatewayApi.post("/v1/auth/login", {
       username,
     });
   },
 
   // Create TOTP login request
   CreateLoginRequestTOTP(username: string, token: string) {
-    return gatewayApi.post("auth/v1/login-totp", {
+    return gatewayApi.post("/v1/auth/login-totp", {
       username,
       token,
     });
@@ -33,21 +33,21 @@ export default {
 
   // Get login request status
   LoginRequestStatus(tokenId: string) {
-    return gatewayApi.post("auth/v1/login/approve/status", {
+    return gatewayApi.post("/v1/auth/login/approve/status", {
       token_id: tokenId,
     });
   },
 
   // Approve a login request
   ApproveLogin(token: string) {
-    return gatewayApi.post("auth/v1/login/approve", {
+    return gatewayApi.post("/v1/auth/login/approve", {
       token,
     });
   },
 
   // Create a login session (access token)
   CreateLoginSession(username: string, tokenId: string, signature: string) {
-    return gatewayApi.post("auth/v1/login/create-session", {
+    return gatewayApi.post("/v1/auth/login/create-session", {
       username: username,
       token_id: tokenId,
       signature: signature,
@@ -56,44 +56,44 @@ export default {
 
   // Send encrypted account payload
   CreateAccount(account: EncryptedAccount) {
-    return gatewayApi.post("auth/v1/register", account);
+    return gatewayApi.post("/v1/auth/register", account);
   },
 
   // Get ephemeral register token
   GetRegisterToken() {
-    return gatewayApi.get("auth/v1/register/new");
+    return gatewayApi.get("/v1/auth/register/new");
   },
 
   // Get encrypted account payload
   GetAccount() {
-    return gatewayApi.get("auth/v1/get-account");
+    return gatewayApi.get("/v1/auth/get-account");
   },
 
   // Get a new token to sign for refresh session
   GetRefreshSessionToken() {
-    return gatewayApi.get("auth/v1/refresh-session/new");
+    return gatewayApi.get("/v1/auth/refresh-session/new");
   },
 
   // Submit signed token to receive session token
   SubmitSignedSessionToken(token: SignedSessionToken) {
-    return gatewayApi.post("auth/v1/refresh-session", token);
+    return gatewayApi.post("/v1/auth/refresh-session", token);
   },
 
   // Verify email address
   VerifyEmail(token: string) {
-    return gatewayApi.post("auth/v1/account/verify-email", {
+    return gatewayApi.post("/v1/auth/account/verify-email", {
       token,
     });
   },
 
   // Resend email for account verification
   ResendEmailVerification() {
-    return gatewayApi.post("auth/v1/account/resend-verification-email");
+    return gatewayApi.post("/v1/auth/account/resend-verification-email");
   },
 
   // Enable TOTP
   EnableTOTP(secret: string, token: string) {
-    return gatewayApi.post("auth/v1/account/enable-totp", {
+    return gatewayApi.post("/v1/auth/account/enable-totp", {
       secret,
       token,
     });
@@ -101,26 +101,26 @@ export default {
 
   // Disable TOTP
   DisableTOTP() {
-    return gatewayApi.post("auth/v1/account/disable-totp");
+    return gatewayApi.post("/v1/auth/account/disable-totp");
   },
 
   // Get account recovery codes
   GetRecoveryCodes() {
-    return gatewayApi.get("auth/v1/account/recovery-codes");
+    return gatewayApi.get("/v1/auth/account/recovery-codes");
   },
 
   // Delete account
   DeleteAccount() {
-    return gatewayApi.post("auth/v1/account/delete");
+    return gatewayApi.post("/v1/auth/account/delete");
   },
 
   // Change password request token
   ChangePasswordToken() {
-    return gatewayApi.get("/auth/v1/account/change-password");
+    return gatewayApi.get("/v1/auth/account/change-password");
   },
 
   // Change password
   ChangePassword(payload) {
-    return gatewayApi.post("/auth/v1/account/change-password", payload);
+    return gatewayApi.post("/v1/auth/account/change-password", payload);
   },
 };

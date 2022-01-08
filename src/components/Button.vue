5e87c46a961547c0ce23b8884955117c5942e0cd
Jack Coble <jack@coble.uk>
2022-01-08 16:04:12 +0000

    New gradient background style for buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,16 @@
 <template>
   <button
     :disabled="loading"
-    class="transition duration-300 ease-in-out px-3 py-2 w-36 text-sm border rounded-md focus:outline-none disabled:opacity-50"
-    :class="
-      isLight
-        ? 'bg-gray-50 border hover:bg-gray-200'
-        : 'text-yellow-900 bg-orange-500 border-orange-500 hover:bg-gray-100 hover:text-gray-900 hover:border-gray-200'
+    class="
+      px-3
+      py-2
+      text-white
+      rounded-md
+      focus:outline-none
+      bg-gradient-to-r
+      from-orange-600
+      via-orange-500
+      to-yellow-500
     "
   >
     <slot v-if="!loading">Button</slot>
@@ -20,10 +25,6 @@ export default defineComponent({
   name: "Button",
   props: {
     loading: {
-      type: Boolean,
-      default: false,
-    },
-    isLight: {
       type: Boolean,
       default: false,
     },

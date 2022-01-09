34e237397e394fa553f79d7a7b839fa4babc853a
Jack Coble <jack@coble.uk>
2022-01-09 11:54:45 +0000

    Add a light mode to button component

--- src/components/elements/Button.vue
+++ src/components/elements/Button.vue
@@ -1,7 +1,8 @@
 <template>
     <button
         :disabled="loading"
-        class="transition duration-300 ease-in-out px-3 py-2 w-36 text-sm font-medium border rounded-md focus:outline-none disabled:opacity-50 text-yellow-900 bg-orange-500 border-orange-500 hover:bg-gray-100 hover:text-gray-900 hover:border-gray-200"
+        class="transition duration-300 ease-in-out px-3 py-2 w-36 text-sm font-medium border rounded-md focus:outline-none disabled:opacity-50"
+        :class="light ? 'bg-gray-100 border-gray-200 hover:bg-gray-200 hover:border-gray-100 text-gray-700' : 'bg-orange-500 border-orange-500 hover:bg-gray-100 hover:text-gray-900 hover:border-gray-200 text-yellow-900'"
     >
         <slot v-if="!loading">Button</slot>
         <svg
@@ -37,6 +38,10 @@ export default defineComponent({
         loading: {
             type: Boolean,
             default: false,
+        },
+        light: {
+            type: Boolean,
+            default: false
         }
     },
 });

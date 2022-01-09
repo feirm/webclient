44b5386a24ebac1c883787f7244dd5896ad241f3
Jack Coble <jack@coble.uk>
2022-01-09 12:08:58 +0000

    Update Button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,33 +1,48 @@
 <template>
-  <button
-    :disabled="loading"
-    class="
-      px-3
-      py-2
-      text-white
-      rounded-md
-      focus:outline-none
-      bg-gradient-to-r
-      from-orange-600
-      via-orange-500
-      to-yellow-500
-    "
-  >
-    <slot v-if="!loading">Button</slot>
-    <img v-else class="mx-auto w-5" src="@/assets/loading_spinner.svg" />
-  </button>
+    <button
+        :disabled="loading"
+        class="transition duration-300 ease-in-out px-3 py-2 w-36 text-sm font-medium border rounded-md focus:outline-none disabled:opacity-50"
+        :class="light ? 'bg-gray-100 border-gray-200 hover:bg-gray-200 hover:border-gray-100 text-gray-700' : 'bg-orange-500 border-orange-500 hover:bg-gray-100 hover:text-gray-900 hover:border-gray-200 text-yellow-900'"
+    >
+        <slot v-if="!loading">Button</slot>
+        <svg
+            v-else
+            class="animate-spin mx-auto h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+            />
+            <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            />
+        </svg>
+    </button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Button",
-  props: {
-    loading: {
-      type: Boolean,
-      default: false,
+    name: "Button",
+    props: {
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+        light: {
+            type: Boolean,
+            default: false
+        }
     },
-  },
 });
 </script>

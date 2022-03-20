667d7170f6087ff560dc45f6d429de68f4ee636e
Jack Coble <jack@coble.uk>
2022-03-20 17:58:45 +0000

    Update user service to correctly return Base64 encoded image

--- src/service/api/userService.ts
+++ src/service/api/userService.ts
@@ -1,8 +1,16 @@
 import { gatewayApi } from "./api";
 
 export default {
+    // Returns Base64 encoded string of avatar
     GetAvatar(username?: string): Promise<string> {
-        return gatewayApi.get(`/v1/user/avatar/${username}`)
-            .then(response => Buffer.from(response.data, "binary").toString("base64"))
+        const url = username ? `/v1/user/avatar/${username}` : `/v1/user/avatar`;
+        return gatewayApi.get(url, { responseType: "arraybuffer" })
+            .then(response => {
+                const contentType = response.headers["content-type"];
+                const b64 = Buffer.from(response.data, "binary").toString("base64")
+
+                const encodedString = `data:${contentType};base64,${b64}`;
+                return Promise.resolve(encodedString);
+            })
     }
 }

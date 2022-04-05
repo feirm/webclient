8193626a6864ceb8852f111a3fce1dd522b286e6
Jack Coble <jack@coble.uk>
2022-04-05 21:48:52 +0100

    Re-import solid icon library

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,10 @@ import "@/css/index.css";
 // FontAwesome icons
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(far);
+library.add(far, fas);
 
 // Custom components
 import VueHcaptcha from "@hcaptcha/vue3-hcaptcha";

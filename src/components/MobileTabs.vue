cdb902a636750f68e076d430d7e5b6ac11a6b7f1
Jack Coble <jack@coble.uk>
2022-04-05 21:16:49 +0100

    More refactoring of mobile tabs component

--- src/components/MobileTabs.vue
+++ src/components/MobileTabs.vue
@@ -4,22 +4,7 @@
         <router-link :to="button.route" class="flex flex-col flex-grow items-center justify-center
 		overflow-hidden whitespace-no-wrap text-sm transition-colors duration-100
 		ease-in-out hover:bg-gray-200 focus:text-orange-500">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round">
-                <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
-                <path
-                    d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
-                    2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
-            </svg>
-
+            <font-awesome-icon class="fa-2xl" :icon="button.icon" />
             <span class="text-sm">{{ button.label }}</span>
         </router-link>
     </div>
